409d26bef69f0eed7a1429c228975d335dc4b909
mdk <qrvmil@yandex.ru>
2021-10-29 22:02:02 +0300

    new_02

--- PROJECT/GraphLine_PyQt5.py
+++ PROJECT/GraphLine_PyQt5.py
@@ -31,11 +31,11 @@ class DrawStar(QWidget):
         self.button_2.clicked.connect(self.masplus)
 
     def masmin(self):
-        self.mas *= 1.3
+        self.mas *= 1.2
         self.repaint()
 
     def masplus(self):
-        self.mas /= 1.3
+        self.mas /= 1.2
         self.repaint()
 
     def paintEvent(self, event):
